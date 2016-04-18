0ab97a5c253c9f31c7b6fa23ec7723e909e870a2
Doug Bergh <dougbergh@gmail.com>
2016-04-18 16:45:23 -0700

    cleanup

--- web-app/public/js/core.js
+++
@@ -1,2 +0,0 @@
-angular.module('testrun', ['testrunController', 'testrunService', 
-			   'testexecutorService', 'sendemailService' ]);
--- web-app/public/js/services/sendemail.js
+++
@@ -1,9 +0,0 @@
-angular.module('sendemailService', [])
-
-    .factory('EmailSender', ['$http',function($http) {
-	return {
-	    create : function(data) {
-		return $http.post('/api/emailsender', data);
-	    }
-	}
-    }]);
